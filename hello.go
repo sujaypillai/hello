1c1ed575a3958d321d7de7ac5e627e732b7cb3cd
Sujay Pillai <sujayopillai@gmail.com>
2020-02-14 21:30:10 +0800

    Added simple go code and Dockerfile for buildx demo

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
+
+func main() {
+	hostname, _ := os.Hostname()
+	fmt.Printf("Hello, I am %s running on %s %s\n", hostname, runtime.GOOS, runtime.GOARCH)
+}
